cf99171b4ff0f47015b131c10bfe418f5b8bc264
Oganes Bozoyan <ogi@mail.ru>
2022-10-06 16:36:30 +0300

    upload image_test

---
+++ main.py
@@ -0,0 +1,15 @@
+from git_api import *
+
+if __name__ == "__main__":
+    # =================== variables =============================
+    url_dataset = "https://api.github.com/repos/ogbozoyan/Dataset/contents"
+    us_name = "ogbozoyan"
+    token = "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"
+    who_work_now = "Oganes Bozoyan"
+    who_work_now_mail = "ogi@mail.ru"
+    # ===========================================================
+    path = os.getcwd()
+    fnames = find_fls(path, ".py")
+    print(fnames)
+    for i in fnames:
+        upload(url_dataset, us_name, token, who_work_now, who_work_now_mail, i[40:])
